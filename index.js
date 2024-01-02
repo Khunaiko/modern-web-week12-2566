676cc0d425ddd4476805848bcecc87428bae27e9
Khunaiko <pasuaiko21089@gmail.com>
2024-01-02 16:04:37 +0700

    update

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-const { config } = require('dotenv');
 const express = require('express');
 const logger = require('morgan')
-require('.dotenv'),config()
+require('dotenv').config()
 const app = express();
 
-//HTTP request logger middlware
+//HTTP request logger middleware
 app.use(logger("dev"));
 app.use(express.json())
 
 //http://localhost:3000
-
-app.get('/user',(req, res) =>{
-    res.status(200).json({message: "GET request to the homepage"});
-})
-
-app.post('/user',(req, res) =>{
+  app.get('/',(req,res)=> {
+    res.status(200).json({ message: 'GET request to the homepage'});
+  })
+  app.post('/user',(req,res) => {
     const data = req.body
-    res.status(201).json({massage: data});
-})
+    res.status(201).json({ message: data});
+  })
+  app.put('/user',(req,res) => {
+    res.status(200).json({ message: 'PUT request to the homepage'});
+  })
+  app.delete('/user',(req,res) => {
+  res.status(200).json({ message: 'DELETE request to the homepage'});
+  })
 
-app.put('/user',(req, res) =>{
-    res.status(200).json({massage: "PUT request to the homepage"});
-})
-
-app.delete('/user',(req, res) =>{
-    res.status(200).json({massage: "DELETE request to the homepage"});
-})
-
-const port = process.env.PORT || 5000 ;
-app.listen(port,() => {
-    console.log(`Server runing at http://localhost:${port}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
